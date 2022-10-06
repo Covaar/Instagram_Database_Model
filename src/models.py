a023581639e339f37525d4457730718c8f6d2463
Covaar <co.va.ard@gmail.com>
2022-10-06 22:01:28 +0000

    My first commit

--- src/models.py
+++ src/models.py
@@ -1,33 +1,54 @@
 import os
 import sys
-from sqlalchemy import Column, ForeignKey, Integer, String
+from sqlalchemy import Column, ForeignKey, Integer, String, DateTime
 from sqlalchemy.ext.declarative import declarative_base
 from sqlalchemy.orm import relationship
 from sqlalchemy import create_engine
 from eralchemy import render_er
+from datetime import datetime
 
 Base = declarative_base()
 
-class Person(Base):
-    __tablename__ = 'person'
-    # Here we define columns for the table person
-    # Notice that each column is also a normal Python instance attribute.
+class User(Base):
+    __tablename__='users'
     id = Column(Integer, primary_key=True)
-    name = Column(String(250), nullable=False)
-
-class Address(Base):
-    __tablename__ = 'address'
-    # Here we define columns for the table address.
-    # Notice that each column is also a normal Python instance attribute.
+    username=Column(String(20), nullable=False, unique=True)
+    firstname=Column(String(20), nullable=False)
+    lastname=Column(String(20), nullable=False)
+    email=Column(String(50), nullable=False, unique=True)
+    password=Column(String(10), nullable=False)
+    created_at=Column(DateTime(), default=datetime.now())
+    follower=relationship('Follower',backref='User')
+    post=relationship('Post',backref='User')
+    comment=relationship('Comment',backref='User')
+    
+class Follower(Base):
+    __tablename__='followers'
     id = Column(Integer, primary_key=True)
-    street_name = Column(String(250))
-    street_number = Column(String(250))
-    post_code = Column(String(250), nullable=False)
-    person_id = Column(Integer, ForeignKey('person.id'))
-    person = relationship(Person)
+    user_from_id=Column(Integer, ForeignKey('users.id'), nullable=False)
+    user_to_id=Column(Integer, ForeignKey('users.id'), nullable=False)
 
-    def to_dict(self):
-        return {}
+class Post(Base):
+    __tablename__ = 'posts'
+    id = Column(Integer, primary_key=True)
+    user_id= Column(Integer, ForeignKey('users.id'), nullable=False)
+    media=relationship('Media',backref='Post', uselist=False)
+    comment=relationship('Comment',backref='Post', uselist=False)
+
+class Media(Base):
+    __tablename__ = 'media'
+    id = Column(Integer, primary_key=True)
+    type=Column(String(50), nullable=False)
+    url=Column(String(50), nullable=False)
+    post_id= Column(Integer, ForeignKey('posts.id'), nullable=False)
+    
+class Comment(Base):
+    __tablename__ = 'comment'
+    id = Column(Integer, primary_key=True)
+    comment_text=Column(String(120), nullable=False)
+    author_id=Column(Integer, ForeignKey('users.id'), nullable=False)
+    post_id=Column(Integer, ForeignKey('posts.id'), nullable=False)
+
 
 ## Draw from SQLAlchemy base
 render_er(Base, 'diagram.png')
